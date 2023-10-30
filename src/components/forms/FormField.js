7cbc01e9363f30d57791869d4dfb9caade72e203
tchoforwilson <tchoforwilson@gmail.com>
2023-10-30 15:46:05 +0100

    Added left component for icon

--- src/components/forms/FormField.js
+++ src/components/forms/FormField.js
@@ -1,31 +1,39 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
+import { TextInput } from "react-native-paper";
+import { FloatingLabelInput } from "react-native-floating-label-input";
 import { useFormikContext } from "formik";
-import { AppText } from "../common";
-import { AppTextInput } from "../inputs";
 import FormErrorMessage from "./FormErrorMessage";
 import colors from "../../configurations/colors";
 
-const FormField = ({
-  label,
-  name,
-  placeholder,
-  isMessage = false,
-  ...otherProps
-}) => {
+/**
+ * @breif Render a form field component
+ * @param {String} label Form field label
+ * @param {String} name Form field name
+ * @param {String} iconType Form field icon type
+ * @param {Any} otherProps Form field other properties
+ */
+const FormField = ({ label, name, iconType, ...otherProps }) => {
   const { touched, errors, setFieldValue, values } = useFormikContext();
   return (
     <View style={styles.field}>
-      <AppTextInput
-        placeholder={placeholder}
+      <FloatingLabelInput
+        label={label}
         value={values[name]}
         onChangeText={(text) => setFieldValue(name, text)}
-        style={styles.input}
+        leftComponent={
+          <TextInput.Icon
+            icon={iconType}
+            style={styles.icon}
+            color={colors.grey_dark_1}
+          />
+        }
+        containerStyles={styles.container}
+        labelStyles={styles.label}
+        inputStyles={styles.input}
         {...otherProps}
       />
-      {!isMessage && (
-        <FormErrorMessage error={errors[name]} visible={touched[name]} />
-      )}
+      <FormErrorMessage error={errors[name]} visible={touched[name]} />
     </View>
   );
 };
@@ -35,18 +43,26 @@ const styles = StyleSheet.create({
     width: "100%",
     marginBottom: 15,
   },
+  container: {
+    flex: 1,
+    borderWidth: 1,
+    paddingHorizontal: 15,
+    backgroundColor: colors.white,
+    borderColor: colors.grey_dark_3,
+    borderRadius: 8,
+  },
   label: {
-    fontWeight: 600,
-    fontSize: 20,
-    textAlign: "left",
-    marginBottom: 5,
+    marginLeft: 40,
+    color: colors.grey_dark_3,
   },
   input: {
-    borderColor: colors.grey_light_3,
-    backgroundColor: colors.grey_light_3,
-    borderWidth: 1,
-    borderRadius: 5,
-    padding: 8,
+    color: colors.black,
+    marginLeft: 40,
+    paddingVertical: 15,
+    fontSize: 20,
+  },
+  icon: {
+    marginLeft: 30,
   },
 });
 
