b99366da5398c622150345b379e763bdd19cb966
tchoforwilson <tchoforwilson@gmail.com>
2023-10-30 15:41:18 +0100

    Authentication navigator for Login, Register and Welcome screens

---
+++ src/navigations/AuthNavigator.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { LoginScreen, RegisterScreen, WelcomeScreen } from "../screens";
+
+const Stack = createNativeStackNavigator();
+
+const AuthNavigator = () => (
+  <Stack.Navigator>
+    <Stack.Screen
+      name="Welcome"
+      component={WelcomeScreen}
+      options={{ headerShown: false }}
+    />
+    <Stack.Screen name="Login" component={LoginScreen} />
+    <Stack.Screen name="Register" component={RegisterScreen} />
+  </Stack.Navigator>
+);
+
+export default AuthNavigator;
