50eda86415a8274fa6a6fa2ad17f5021634095af
tchoforwilson <tchoforwilson@gmail.com>
2023-10-30 15:39:21 +0100

    Added reuse styles for text and headings

---
+++ src/configurations/styles.js
@@ -0,0 +1,28 @@
+import { Platform } from "react-native";
+
+import colors from "./colors";
+
+export default {
+  colors,
+  text: {
+    color: colors.dark,
+    fontSize: 18,
+    fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
+  },
+  heading: {
+    width: "100%",
+    alignItems: "flex-start",
+    textAlign: "center",
+    marginBottom: 30,
+    primary: {
+      fontSize: 25,
+      fontWeight: "700",
+    },
+    secondary: {
+      color: colors.grey_dark_3,
+      fontSize: 18,
+      fontWeight: "400",
+      marginTop: 10,
+    },
+  },
+};
