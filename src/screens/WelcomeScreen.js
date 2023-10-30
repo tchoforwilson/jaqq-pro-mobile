c79c32c8a3311bb53fc3e65f91c995b36fc55543
tchoforwilson <tchoforwilson@gmail.com>
2023-10-30 19:11:18 +0100

    Implemented Welcome screen and reate stack navigation

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,25 +1,61 @@
 import React from "react";
-
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, ImageBackground } from "react-native";
+import { AppText } from "../components/common";
+import routes from "../navigations/routes";
 import colors from "../configurations/colors";
 import { AppButton } from "../components/buttons";
 
-const WelcomeScreen = () => {
+const WelcomeScreen = ({ navigation }) => {
   return (
-    <View style={styles.container}>
-      <Text>Welcome to Jaqq pro</Text>
-      <AppButton title="get started" color={colors.primary} />
-    </View>
+    <ImageBackground
+      style={styles.background}
+      source={require("../../assets/background.jpg")}
+    >
+      <View style={styles.logoContainer}>
+        <Text style={styles.logo}>Japp Pro</Text>
+        <AppText style={styles.tagLine}>
+          Find the perfect place for work
+        </AppText>
+      </View>
+      <View style={styles.buttonsContainer}>
+        <AppButton
+          title="Login"
+          onPress={() => navigation.navigate(routes.LOGIN)}
+        />
+        <AppButton
+          title="Register"
+          onPress={() => navigation.navigate(routes.REGISTER)}
+        />
+      </View>
+    </ImageBackground>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  background: {
     flex: 1,
-    backgroundColor: colors.white,
+    justifyContent: "flex-end",
     alignItems: "center",
-    justifyContent: "center",
-    paddingHorizontal: 24,
+    objectFit: "cover",
+    resize: "both",
+    objectPosition: "center",
+  },
+  logo: {
+    fontSize: 25,
+    fontWeight: "700",
+    color: colors.green,
+  },
+  tagLine: {
+    color: colors.grey_dark_1,
+  },
+  logoContainer: {
+    position: "absolute",
+    top: 70,
+    alignItems: "center",
+  },
+  buttonsContainer: {
+    padding: 10,
+    width: "100%",
   },
 });
 
