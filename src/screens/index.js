727acb5b913c5b24f05f46d2ab2cbf2b1c85d9a9
tchoforwilson <tchoforwilson@gmail.com>
2023-11-01 13:51:17 +0100

    Exported all screens

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,3 +1,5 @@
 export { default as WelcomeScreen } from "./WelcomeScreen";
 export { default as LoginScreen } from "./LoginScreen";
 export { default as RegisterScreen } from "./RegisterScreen";
+export { default as AddPhoneNumberScreen } from "./AddPhoneNumberScreen";
+export { default as ConfirmPhoneNumberScreen } from "./ConfirmPhoneNumberScreen";
