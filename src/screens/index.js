f0829491e467470f18e845b157ad81043fa63e2f
tchoforwilson <tchoforwilson@gmail.com>
2024-03-02 16:30:24 +0100

    Exported newly created screens

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,5 +1,13 @@
+/** AUTHENTICATION */
 export { default as WelcomeScreen } from "./WelcomeScreen";
 export { default as LoginScreen } from "./LoginScreen";
 export { default as RegisterScreen } from "./RegisterScreen";
 export { default as AddPhoneNumberScreen } from "./AddPhoneNumberScreen";
 export { default as ConfirmPhoneNumberScreen } from "./ConfirmPhoneNumberScreen";
+/** SERVICES */
+export { default as ServicesScreen } from "./ServicesScreen";
+export { default as ServiceDetailsScreen } from "./ServiceDetailsScreen";
+/** ACCOUNT */
+export { default as AccountScreen } from "./AccountScreen";
+export { default as UpdatePasswordScreen } from "./UpdatePasswordScreen";
+export { default as UpdatePhoneNumberScreen } from "./UpdatePhoneNumberScreen";
