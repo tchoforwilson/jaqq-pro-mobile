06b5a7af3a619b9eafb14f4d5b9ad8d25c93245d
tchoforwilson <tchoforwilson@gmail.com>
2024-03-02 16:29:59 +0100

    Index file

--- src/navigations/index.js
+++ src/navigations/index.js
@@ -1,3 +1,5 @@
+export { default as AppNavigator } from "./AppNavigator";
 export { default as AuthNavigator } from "./AuthNavigator";
+export { default as FeedNavigator } from "./FeedNavigator";
 export { default as NavigationTheme } from "./navigationTheme";
 export { default as routes } from "./routes";
