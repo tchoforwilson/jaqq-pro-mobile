332659daf188d622ca1b92287316f13b79ad4927
tchoforwilson <tchoforwilson@gmail.com>
2024-03-03 16:54:44 +0100

    Export all screens

--- src/screens/index.js
+++ src/screens/index.js
@@ -4,10 +4,14 @@ export { default as LoginScreen } from "./LoginScreen";
 export { default as RegisterScreen } from "./RegisterScreen";
 export { default as AddPhoneNumberScreen } from "./AddPhoneNumberScreen";
 export { default as ConfirmPhoneNumberScreen } from "./ConfirmPhoneNumberScreen";
-/** SERVICES */
-export { default as ServicesScreen } from "./ServicesScreen";
-export { default as ServiceDetailsScreen } from "./ServiceDetailsScreen";
+
+/** TASKS */
+export { default as TasksScreen } from "./TasksScreen";
+export { default as TaskDetailsScreen } from "./TaskDetailsScreen";
+
 /** ACCOUNT */
 export { default as AccountScreen } from "./AccountScreen";
+export { default as ProfileScreen } from "./ProfileScreen";
+export { default as ServicesScreen } from "./ServicesScreen";
 export { default as UpdatePasswordScreen } from "./UpdatePasswordScreen";
 export { default as UpdatePhoneNumberScreen } from "./UpdatePhoneNumberScreen";
