f5d27b6ec4ea5705b3180246518f65341a9e0453
tchoforwilson <tchoforwilson@gmail.com>
2024-03-05 03:16:14 +0100

    Card component

--- src/components/cards/Card.js
+++ src/components/cards/Card.js
@@ -1,41 +1,44 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, TouchableWithoutFeedback } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import { AppText } from "../common";
-
 import colors from "../../configurations/colors";
 
-function Card({ title, subTitle, location }) {
+const Card = ({ title, subTitle, location, onPress }) => {
   return (
-    <View styles={styles.card}>
-      <AppText style={styles.title}>{title}</AppText>
-      <AppText style={styles.subtitle}>{subTitle}</AppText>
-      <View style={styles.locationContainer}>
-        <MaterialCommunityIcons
-          name="map-marker"
-          size={25}
-          color={colors.secondary}
-          style={styles.icon}
-        />
-        <AppText style={styles.location}>{location}</AppText>
+    <TouchableWithoutFeedback onPress={onPress}>
+      <View styles={styles.card}>
+        <AppText style={styles.title}>{title}</AppText>
+        <AppText style={styles.subtitle}>{subTitle}</AppText>
+        <View style={styles.locationContainer}>
+          <MaterialCommunityIcons
+            name="map-marker"
+            size={25}
+            color={colors.secondary}
+            style={styles.icon}
+          />
+          <AppText style={styles.location}>{location}</AppText>
+        </View>
       </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
-}
+};
 
 const styles = StyleSheet.create({
   card: {
     borderRadius: 15,
     backgroundColor: colors.white,
     marginBottom: 20,
+    overflow: "hidden",
   },
   locationContainer: {
     display: "flex",
     flexDirection: "row",
     alignItems: "center",
     gap: 12,
-    marginTop: 20,
+    marginTop: 15,
+    padding: 20,
   },
   location: {
     color: colors.grey_dark_1,
@@ -43,12 +46,14 @@ const styles = StyleSheet.create({
   icon: {
     color: colors.secondary,
   },
+  subtitle: {
+    color: colors.secondary,
+    fontWeight: "bold",
+  },
   title: {
     color: colors.primary,
     textTransform: "capitalize",
-  },
-  subtitle: {
-    color: colors.secondary,
+    marginBottom: 7,
   },
 });
 
