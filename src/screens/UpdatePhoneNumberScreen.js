d892621e16da1e8df647868d4f51cffda1bc33d6
tchoforwilson <tchoforwilson@gmail.com>
2024-03-05 03:15:48 +0100

    Implementation for update Phone number screen

--- src/screens/UpdatePhoneNumberScreen.js
+++ src/screens/UpdatePhoneNumberScreen.js
@@ -1,17 +1,69 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
-import { AppText } from "../components/common";
+import * as Yup from "yup";
 
-function UpdatePhoneNumberScreen(props) {
+import { AppScreen, AppText } from "../components/common";
+import { FormContainer, FormField } from "../components/forms";
+import { SubmitButton } from "../components/buttons";
+
+const validationSchema = Yup.object().shape({
+  phoneCurrent: Yup.number()
+    .integer()
+    .typeError("Number must be an integer")
+    .test("is-nine-digits", "Number must be 9 digits", (value) => {
+      if (value) {
+        const stringValue = String(value);
+        return stringValue.length === 9;
+      }
+      return false;
+    })
+    .label("current Phone Number"),
+  phoneNew: Yup.number()
+    .integer()
+    .typeError("Number must be an integer")
+    .test("is-nine-digits", "Number must be 9 digits", (value) => {
+      if (value) {
+        const stringValue = String(value);
+        return stringValue.length === 9;
+      }
+      return false;
+    })
+    .label("New Phone Number"),
+});
+const UpdatePhoneNumberScreen = () => {
+  const handleSubmit = (data) => {
+    console.log(data);
+  };
   return (
-    <View style={styles.container}>
-      <AppText>Change your phone number</AppText>
-    </View>
+    <AppScreen style={styles.screen}>
+      <View style={styles.container}>
+        <FormContainer
+          initialValues={{ phoneCurrent: "", phoneNew: "" }}
+          validationSchema={validationSchema}
+          onSubmit={handleSubmit}
+        >
+          <FormField
+            label="Old Phone Number"
+            name="phoneCurrent"
+            iconType="phone"
+            keyboardType="numeric"
+          />
+          <FormField
+            label="New Phone Number"
+            name="phoneNew"
+            iconType="phone"
+            keyboardType="numeric"
+          />
+          <SubmitButton title="Change number" />
+        </FormContainer>
+      </View>
+    </AppScreen>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {},
+  screen: {},
 });
 
 export default UpdatePhoneNumberScreen;
