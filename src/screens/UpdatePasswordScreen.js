cd0496d264b6f19ccdaba9ac055ea9f6dc51a7c7
tchoforwilson <tchoforwilson@gmail.com>
2024-03-05 03:15:10 +0100

    Implementation for provider password update

--- src/screens/UpdatePasswordScreen.js
+++ src/screens/UpdatePasswordScreen.js
@@ -1,16 +1,53 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
-import { AppText } from "../components/common";
+import * as Yup from "yup";
 
-function UpdatePasswordScreen(props) {
+import { AppScreen } from "../components/common";
+import { FormContainer, FormPasswordField } from "../components/forms";
+import { SubmitButton } from "../components/buttons";
+
+const validationSchema = Yup.object().shape({
+  passwordCurrent: Yup.string()
+    .required("Current password is required!")
+    .label("Current Password"),
+  password: Yup.string()
+    .required("New password is required!")
+    .min(8, "Password must be at least 8 characters")
+    .label("New Password"),
+  passwordConfirm: Yup.string()
+    .required("Confirm new password")
+    .min(8)
+    .label("Current Password"),
+});
+
+const UpdatePasswordScreen = () => {
+  const handleSubmit = (data) => {
+    console.log(data);
+  };
   return (
-    <View style={styles.container}>
-      <AppText>Update your password</AppText>
-    </View>
+    <AppScreen style={styles.screen}>
+      <View style={styles.container}>
+        <FormContainer
+          initialValues={{
+            passwordCurrent: "",
+            password: "",
+            passwordConfirm: "",
+          }}
+          validationSchema={validationSchema}
+          onSubmit={handleSubmit}
+        >
+          <FormPasswordField label="Current Password" name="passwordCurrent" />
+          <FormPasswordField label="New Password" name="password" />
+          <FormPasswordField label="Confirm Password" name="passwordConfirm" />
+          <SubmitButton title="change password" />
+        </FormContainer>
+      </View>
+    </AppScreen>
   );
-}
+};
 
 const styles = StyleSheet.create({
+  screen: {},
   container: {},
 });
 
