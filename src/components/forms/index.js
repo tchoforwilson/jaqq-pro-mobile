0da4e318399517edc1eb53ab496869a618d8312c
tchoforwilson <tchoforwilson@gmail.com>
2024-03-09 11:10:44 +0100

    Exported FormGender field

--- src/components/forms/index.js
+++ src/components/forms/index.js
@@ -1,4 +1,5 @@
 export { default as FormContainer } from "./FormContainer";
+export { default as FormErrorMessage } from "./FormErrorMessage";
 export { default as FormField } from "./FormField";
 export { default as FormPasswordField } from "./FormPasswordField";
-export { default as FormErrorMessage } from "./FormErrorMessage";
+export { default as FormGenderField } from "./FormGenderField";
