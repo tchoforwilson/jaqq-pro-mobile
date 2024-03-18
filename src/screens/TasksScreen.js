7b6c14b4c5e8690f309e380fa5ce31799f849d51
tchoforwilson <tchoforwilson@gmail.com>
2024-03-18 23:58:25 +0100

    Changed layout

--- src/screens/TasksScreen.js
+++ src/screens/TasksScreen.js
@@ -1,10 +1,17 @@
-import React from "react";
-import { StyleSheet, FlatList } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  FlatList,
+  View,
+  TouchableWithoutFeedback,
+} from "react-native";
+import PropTypes from "prop-types";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-import { AppScreen } from "../components/common";
+import { AppScreen, AppText } from "../components/common";
 import colors from "../configurations/colors";
-import { Card } from "../components/cards";
 import routes from "../routes";
+import { FilterTaskModal } from "../components/modals";
 
 const tasks = [
   {
@@ -12,36 +19,92 @@ const tasks = [
     name: "Wedding Photographer",
     price: 30000,
     location: "Bonapriso, douala",
+    user: {
+      name: "Jonas James Freeman",
+      phone: "655448900",
+    },
   },
   {
     id: 2,
     name: "Carpenter",
     price: 80000,
     location: "Ndokoti, douala",
+    user: {
+      name: "Mary Anne",
+      phone: "655411900",
+    },
   },
   {
     id: 3,
     name: "Builder",
     price: 500000,
     location: "Bonaberi, douala",
+    user: {
+      name: "Ricky Brown",
+      phone: "655448900",
+    },
   },
 ];
 
+const TaskItem = ({ task, onPress }) => {
+  return (
+    <TouchableWithoutFeedback onPress={onPress}>
+      <View style={styles.taskitem}>
+        <View style={styles.detialsContainer}>
+          <AppText style={styles.name}>{task.name}</AppText>
+          <AppText style={styles.price}>{task.price + " XAF"}</AppText>
+        </View>
+        <View style={styles.locationContainer}>
+          <MaterialCommunityIcons
+            name="map-marker"
+            size={25}
+            color={colors.primary}
+            style={styles.icon}
+          />
+          <AppText style={styles.location}>{task.location}</AppText>
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+};
+
+TaskItem.propTypes = {
+  task: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    location: PropTypes.string.isRequired,
+  }),
+  onPress: PropTypes.func.isRequired,
+};
+
 const TasksScreen = ({ navigation }) => {
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const handleSubmit = (data) => {
+    console.log(data);
+  };
   return (
     <AppScreen style={styles.screen}>
-      <FlatList
-        data={tasks}
-        keyExtractor={(task) => task.id.toString()}
-        renderItem={({ item }) => (
-          <Card
-            title={item.name}
-            subTitle={item.price + " XAF"}
-            location={item.location}
-            onPress={() => navigation.navigate(routes.TASK_DETAILS, item)}
-          />
-        )}
-      />
+      <View style={styles.header}>
+        <MaterialCommunityIcons
+          name="filter"
+          size={28}
+          color={colors.grey_dark_1}
+        />
+      </View>
+      <View style={styles.container}>
+        <FlatList
+          data={tasks}
+          keyExtractor={(task) => task.id.toString()}
+          renderItem={({ item }) => (
+            <TaskItem
+              task={item}
+              onPress={() => navigation.navigate(routes.TASK_DETAILS, item)}
+            />
+          )}
+        />
+      </View>
     </AppScreen>
   );
 };
@@ -49,6 +112,44 @@ const TasksScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   screen: {
     backgroundColor: colors.light,
+  },
+  header: {
+    justifySelf: "flex-end",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "flex-end",
+    marginBottom: 10,
+  },
+  container: {},
+  taskitem: {
+    backgroundColor: colors.white,
+    borderRadius: 10,
+    marginVertical: 7,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+  },
+  detialsContainer: {
+    marginBottom: 15,
+  },
+  name: {
+    fontWeight: 800,
+    fontSize: 18,
+    marginBottom: 5,
+    color: colors.primary,
+  },
+  price: {
+    fontWeight: 500,
+    color: colors.secondary,
+  },
+  locationContainer: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    gap: 10,
+  },
+  location: {
+    color: colors.grey_dark_2,
   },
 });
 
