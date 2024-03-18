bdfbbab4a2611f62609a0c7ffb164033a44f76a4
tchoforwilson <tchoforwilson@gmail.com>
2024-03-18 23:58:59 +0100

    Exported FilterTaskModal

--- src/components/modals/index.js
+++ src/components/modals/index.js
@@ -1,2 +1,3 @@
 export { default as AppModal } from "./AppModal";
 export { default as UpdateServicesModal } from "./UpdateServicesModal";
+export { default as FilterTaskModal } from "./FilterTaskModal";
