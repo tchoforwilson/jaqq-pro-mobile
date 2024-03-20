d6329206b1b47b8be2a0ada3d5a841ba22a4efa2
tchoforwilson <tchoforwilson@gmail.com>
2024-03-20 08:45:22 +0100

    change api URL

--- src/services/http.services.js
+++ src/services/http.services.js
@@ -2,7 +2,7 @@ import { create } from "apisauce";
 
 // Define the API
 const api = create({
-  baseURL: "http://localhost:9000/api",
+  baseURL: "http://192.168.1.153:9000/api",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
