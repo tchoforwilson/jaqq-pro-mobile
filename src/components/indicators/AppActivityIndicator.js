943d138b6229a8a47125eabf79a21d1c7ddc25d2
tchoforwilson <tchoforwilson@gmail.com>
2024-03-22 07:17:58 +0100

    Wrap in a view to occupy all screen

--- src/components/indicators/AppActivityIndicator.js
+++ src/components/indicators/AppActivityIndicator.js
@@ -1,15 +1,30 @@
 import React from "react";
 import LottieView from "lottie-react-native";
+import { View, StyleSheet } from "react-native";
+import colors from "../../configurations/colors";
 
 const AppActivityIndicator = ({ visible = false }) => {
   if (!visible) return null;
   return (
-    <LottieView
-      autoPlay
-      loop
-      source={require("../../assets/animations/loader.json")}
-    />
+    <View style={styles.overlay}>
+      <LottieView
+        autoPlay
+        loop
+        source={require("../../assets/animations/loader.json")}
+      />
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  overlay: {
+    position: "absolute",
+    backgroundColor: colors.white,
+    height: "100%",
+    opacity: 0.8,
+    width: "100%",
+    zIndex: 1,
+  },
+});
 
 export default AppActivityIndicator;
