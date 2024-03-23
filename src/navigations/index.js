e627cd59fac2516f78c73f6daedc56b5736a0bc9
tchoforwilson <tchoforwilson@gmail.com>
2024-03-23 21:05:05 +0100

    Exported root navigation and Register navigator

--- src/navigations/index.js
+++ src/navigations/index.js
@@ -3,4 +3,5 @@ export { default as AppNavigator } from "./AppNavigator";
 export { default as AuthNavigator } from "./AuthNavigator";
 export { default as FeedNavigator } from "./FeedNavigator";
 export { default as NavigationTheme } from "./navigationTheme";
+export { default as RegisterNavigator } from "./RegisterNavigator";
 export { default as RootNavigator } from "./rootNavigation";
