f18762152f2e2092b681f39bd071281a48d60fd3
tchoforwilson <tchoforwilson@gmail.com>
2024-03-29 13:38:01 +0100

    Added modal context

--- App.js
+++ App.js
@@ -6,28 +6,42 @@ import {
   AppNavigator,
   AuthNavigator,
   NavigationTheme,
+  RegisterNavigator,
+  ScreenNavigator,
 } from "./src/navigations";
 import OfflineNotice from "./src/components/common/OfflineNotice";
-import { AuthContext, authStorage } from "./src/context";
+import {
+  AuthContext,
+  ErrorContext,
+  ModalContext,
+  authStorage,
+} from "./src/context";
 import { navigationRef } from "./src/navigations/rootNavigation";
+import { AlertError } from "./src/components/alerts";
 
 const App = () => {
-  const [user, setUser] = useState();
+  const [modalVisible, setModalVisible] = useState(false);
+  const [visible, setVisible] = useState(false);
+  const [status, setStatus] = useState("");
+  const [message, setMessage] = useState("");
+  const [user, setUser] = useState(null);
   const [isReady, setIsReady] = useState(false);
 
-  const restoreUser = async () => {
-    try {
-      await SplashScreen.preventAutoHideAsync();
-      const user = await authStorage.getUser();
-      if (user) setUser(user);
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setIsReady(true);
-    }
-  };
+  const toggleModal = () => setModalVisible(!modalVisible);
 
   useEffect(() => {
+    const restoreUser = async () => {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+        const storedUser = await authStorage.getUser();
+        setUser(storedUser);
+      } catch (error) {
+        console.error("Error restoring user:", error);
+      } finally {
+        setIsReady(true);
+      }
+    };
+
     restoreUser();
   }, []);
 
@@ -41,16 +55,38 @@ const App = () => {
     return null;
   }
 
+  const renderNavigator = () => {
+    if (user && user.phoneValidated) {
+      return <AppNavigator />;
+    } else if (user && !user.phoneValidated) {
+      return <RegisterNavigator />;
+    } else {
+      return <AuthNavigator />;
+    }
+  };
+
   return (
     <AuthContext.Provider value={{ user, setUser }}>
-      <OfflineNotice />
-      <NavigationContainer
-        ref={navigationRef}
-        theme={NavigationTheme}
-        onReady={onLayoutRootView}
+      <ErrorContext.Provider
+        value={{ visible, status, setStatus, message, setMessage, setVisible }}
       >
-        {user ? <AppNavigator /> : <AuthNavigator />}
-      </NavigationContainer>
+        <ModalContext.Provider value={{ modalVisible, toggleModal }}>
+          <OfflineNotice />
+          <AlertError
+            visible={visible}
+            status={status}
+            message={message}
+            onClose={() => setVisible(!visible)}
+          />
+          <NavigationContainer
+            ref={navigationRef}
+            theme={NavigationTheme}
+            onReady={onLayoutRootView}
+          >
+            {renderNavigator()}
+          </NavigationContainer>
+        </ModalContext.Provider>
+      </ErrorContext.Provider>
     </AuthContext.Provider>
   );
 };
