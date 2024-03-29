0801ca805f289e02e777bb45b4b93dc50f8a5284
tchoforwilson <tchoforwilson@gmail.com>
2024-03-29 13:34:57 +0100

    Exported all hooks

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -3,3 +3,5 @@ export { default as useLocation } from "./useLocation";
 export { default as useAuth } from "./useAuth";
 export { default as useModal } from "./useModal";
 export { default as useNotifications } from "./useNotifications";
+export { default as useDate } from "./useDate";
+export { default as useConnection } from "./useConnection";
