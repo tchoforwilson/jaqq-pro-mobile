ee555e042761a75309f9f032b8c9b1278ddff04b
tchoforwilson <tchoforwilson@gmail.com>
2024-03-29 13:33:27 +0100

    Exported all modal contexts

--- src/context/index.js
+++ src/context/index.js
@@ -1,3 +1,4 @@
 export { default as AuthContext } from "./AuthContext";
 export { default as authStorage } from "./storage";
 export { default as ErrorContext } from "./ErrorContext";
+export { default as ModalContext } from "./ModalContext";
