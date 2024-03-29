ff33db5b61f7374b409459b53af2a056d494781c
tchoforwilson <tchoforwilson@gmail.com>
2024-03-29 13:31:37 +0100

    Sockets services

---
+++ src/services/socket.js
@@ -0,0 +1,13 @@
+import { io } from "socket.io-client";
+import storage from "../context/storage";
+
+const handshakeToken = storage.getToken();
+
+const socket = io("http://192.168.1.153:9000", {
+  transports: ["websocket"],
+  auth: {
+    token: handshakeToken,
+  },
+});
+
+export default socket;
