53733dd22332f869295d214262a21ad6960b5379
tchoforwilson <tchoforwilson@gmail.com>
2024-03-29 13:26:06 +0100

    Keyboard avaoiding view container for long form screens like register and profile screen

---
+++ src/components/common/KeyBoardAvoidingViewContainer.js
@@ -0,0 +1,40 @@
+import React from "react";
+import {
+  StyleSheet,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+} from "react-native";
+
+const KeyBoardAvoidingViewContainer = ({ children, style }) => {
+  return (
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <ScrollView
+        style={[styles.content, style]}
+        showsVerticalScrollIndicator={false}
+      >
+        {children}
+      </ScrollView>
+    </KeyboardAvoidingView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+  },
+  content: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+  },
+});
+
+export default KeyBoardAvoidingViewContainer;
