5df77f4c552b6bec4b4cc5daa64d33a4c636c16e
tchoforwilson <tchoforwilson@gmail.com>
2024-03-29 13:32:20 +0100

    Implemented KeyboardAvoidingViewContainer component

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,2 +1,3 @@
 export { default as AppText } from "./AppText";
 export { default as AppScreen } from "./AppScreen";
+export { default as KeyBoardAvoidingViewContainer } from "./KeyBoardAvoidingViewContainer";
