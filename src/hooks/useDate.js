438a27462f798aa36555e0189f5957b43813d4cf
tchoforwilson <tchoforwilson@gmail.com>
2024-04-03 11:57:42 +0100

    Return current data and system locale hook

--- src/hooks/useDate.js
+++ src/hooks/useDate.js
@@ -1,20 +1,43 @@
 import { NativeModules, Platform } from "react-native";
+import moment from "moment";
 
 export default useDate = () => {
-  console.log("here");
   // Get the device locale
-  //   const deviceLocale =
-  //     Platform.OS === "ios"
-  //       ? NativeModules.SettingsManager.settings.AppleLocale ||
-  //         NativeModules.SettingsManager.settings.AppleLanguages[0]
-  //       : NativeModules.I18nManager.localeIdentifier;
+  // const deviceLocale =
+  // Platform.OS === "ios"
+  //   ? NativeModules.SettingsManager.settings.AppleLocale ||
+  //     NativeModules.SettingsManager.settings.AppleLanguages[0]
+  //   : NativeModules.I18nManager.localeIdentifier;
 
   // Format the date based on the device locale
-  //   const today = new Date().toLocaleDateString("en_US", {
-  //     year: "numeric",
-  //     month: "2-digit",
-  //     day: "2-digit",
-  //   });
+  // const today = new Date().toLocaleDateString(deviceLocale, {
+  //   year: "numeric",
+  //   month: "2-digit",
+  //   day: "2-digit",
+  // });
 
-  //   return { today };
+  // Get elapse time
+
+  const moment = require("moment");
+
+  function getElapsedTime(date) {
+    const today = moment();
+    const diffDays = today.diff(moment(date), "days");
+
+    if (diffDays === 0) {
+      return "Today";
+    } else if (diffDays === 1) {
+      return "Yesterday";
+    } else if (diffDays < 7) {
+      return `${diffDays} days ago`;
+    } else if (diffDays === 7) {
+      return "A week ago";
+    } else if (diffDays <= 365) {
+      return "a months ago";
+    } else {
+      return `${Math.floor(diffDays / 365)} years ago`;
+    }
+  }
+
+  return { getElapsedTime };
 };
