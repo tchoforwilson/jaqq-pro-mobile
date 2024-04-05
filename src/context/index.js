f6d085a779ee044105312bc6024af1f3ec109afc
tchoforwilson <tchoforwilson@gmail.com>
2024-04-05 18:50:03 +0100

    Exported all contexts

--- src/context/index.js
+++ src/context/index.js
@@ -2,3 +2,4 @@ export { default as AuthContext } from "./AuthContext";
 export { default as authStorage } from "./storage";
 export { default as ErrorContext } from "./ErrorContext";
 export { default as ModalContext } from "./ModalContext";
+export { default as NotificationContext } from "./NotificationContext";
