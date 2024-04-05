438b5a70e1ea78c5a9312de78974066ce887a24f
tchoforwilson <tchoforwilson@gmail.com>
2024-04-05 18:52:31 +0100

    Update user locationa nd current location

--- src/hooks/useLocation.js
+++ src/hooks/useLocation.js
@@ -1,21 +1,50 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import * as Location from "expo-location";
 import userServices from "../services/user.services";
+import socket from "../services/socket";
+import { ErrorContext } from "../context";
 
 const useLocation = () => {
   const [location, setLocation] = useState();
+  const [currentLocation, setCurrentLocation] = useState();
+  const { setMessage, setVisible, setStatus } = useContext(ErrorContext);
+
+  const updateLocation = async (coords) => {
+    setLocation({
+      coordinates: [coords.latitude, coords.longitude],
+    });
+
+    userServices.updateUserLocation({
+      coordinates: [coords.latitude, coords.longitude],
+    });
+  };
+
+  const updateCurrentLocation = async (coords) => {
+    setCurrentLocation({
+      coordinates: [coords.latitude, coords.longitude],
+    });
+    socket.emit("currentLocation", {
+      coordinates: [coords.latitude, coords.longitude],
+    });
+  };
 
   const getLocation = async () => {
     try {
-      const { granted } = await Location.requestBackgroundPermissionsAsync();
-      if (!granted) return;
-      // const {
-      //   coords: { latitude, longitude },
-      // } = await Location.getLastKnownPositionAsync();
-      //const location = await Location.getLastKnownPositionAsync();
-      console.log(location);
-      setLocation({ latitude, longitude });
-      userServices.updateUserLocation(location);
+      // 1. Grant user location permission
+      const { granted } = await Location.requestForegroundPermissionsAsync();
+      if (!granted) {
+        setMessage("App needs permission to access your location");
+        setStatus("Failed");
+        setVisible(true);
+        return;
+      }
+      // 2. Get user location
+      const location = await Location.getLastKnownPositionAsync();
+      const currentLocation = await Location.getCurrentPositionAsync({});
+
+      //3.  Update user location and current location
+      updateLocation(location.coords);
+      updateCurrentLocation(currentLocation.coords);
     } catch (error) {
       console.log(error);
     }
@@ -25,7 +54,7 @@ const useLocation = () => {
     getLocation();
   }, []);
 
-  return location;
+  return { location, currentLocation };
 };
 
 export default useLocation;
