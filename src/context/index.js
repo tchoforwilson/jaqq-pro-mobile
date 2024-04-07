f6a52278a9973a25c4321d04955d1219a2594267
tchoforwilson <tchoforwilson@gmail.com>
2024-04-07 21:36:53 +0100

    App alert context

--- src/context/index.js
+++ src/context/index.js
@@ -1,5 +1,5 @@
 export { default as AuthContext } from "./AuthContext";
 export { default as authStorage } from "./storage";
-export { default as ErrorContext } from "./ErrorContext";
+export { default as AlertContext } from "./AlertContext";
 export { default as ModalContext } from "./ModalContext";
 export { default as NotificationContext } from "./NotificationContext";
