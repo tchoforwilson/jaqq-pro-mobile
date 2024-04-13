4704c485864231bf981d80ffe3d437809e108467
tchoforwilson <tchoforwilson@gmail.com>
2024-04-13 22:51:43 +0100

    Added appnavigation titles

--- src/navigations/AccountNavigator.js
+++ src/navigations/AccountNavigator.js
@@ -4,28 +4,43 @@ import { createStackNavigator } from "@react-navigation/stack";
 import {
   AccountScreen,
   ProfileScreen,
+  ReviewDetailsScreen,
+  ReviewsScreen,
   ServicesScreen,
   UpdatePasswordScreen,
   UpdatePhoneNumberScreen,
   UpdateProfileImageScreen,
 } from "../screens";
+import { AppHeaderStyle } from "../components/header";
 
 const Stack = createStackNavigator();
 
 const AccountNavigator = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator screenOptions={AppHeaderStyle}>
       <Stack.Screen name="Account" component={AccountScreen} />
       <Stack.Screen name="Profile" component={ProfileScreen} />
       <Stack.Screen name="Services" component={ServicesScreen} />
+      <Stack.Screen name="Reviews" component={ReviewsScreen} />
+      <Stack.Screen
+        name="ReviewDetails"
+        component={ReviewDetailsScreen}
+        options={({ route }) => ({ title: route.params.task.service.title })}
+      />
       <Stack.Screen
         name="UpdateProfileImage"
         component={UpdateProfileImageScreen}
+        options={{ title: "Change photo" }}
       />
-      <Stack.Screen name="UpdatePassword" component={UpdatePasswordScreen} />
       <Stack.Screen
         name="UpdatePhoneNumber"
         component={UpdatePhoneNumberScreen}
+        options={{ title: "Change number" }}
+      />
+      <Stack.Screen
+        name="UpdatePassword"
+        component={UpdatePasswordScreen}
+        options={{ title: "Change password" }}
       />
     </Stack.Navigator>
   );
