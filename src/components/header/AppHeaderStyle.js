6bd83cfa5bdb78fca93334251c1832accf92fa7f
tchoforwilson <tchoforwilson@gmail.com>
2024-04-13 22:51:02 +0100

    Default app header style

---
+++ src/components/header/AppHeaderStyle.js
@@ -0,0 +1,16 @@
+import colors from "../../configurations/colors";
+
+export default AppHeaderStyle = {
+  headerStyle: {
+    backgroundColor: colors.primary,
+    // elevation: 0,
+    // borderBottomWidth: 0,
+    // borderTopWidth: 0,
+  },
+  headerTintColor: colors.white,
+  headerTitleStyle: {
+    fontWeight: "bold",
+    color: colors.white,
+    textAlign: "center",
+  },
+};
