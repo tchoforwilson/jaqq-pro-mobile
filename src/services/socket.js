db75f941c3c59a5ee3dee28ccf5c4f36491d3c93
tchoforwilson <tchoforwilson@gmail.com>
2024-04-19 07:48:46 +0100

    Included more url for various env

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,9 +1,18 @@
 import { io } from "socket.io-client";
 import storage from "../context/storage";
+import * as Device from "expo-device";
+import { Platform } from "react-native";
 
 const handshakeToken = storage.getToken();
+let SOCKET_SERVER_URL = "http://10.0.2.2:9000";
+if (Platform.OS === "ios") {
+  SOCKET_SERVER_URL = "http://localhost:9000";
+}
+if (Device.isDevice) {
+  SOCKET_SERVER_URL = "http://192.168.1.153:9000";
+}
 
-const socket = io("http://192.168.1.153:9000", {
+const socket = io(SOCKET_SERVER_URL, {
   transports: ["websocket"],
   auth: {
     token: handshakeToken,
