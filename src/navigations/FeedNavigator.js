7d6f288f08da83af148484c15878504f8024d7e6
tchoforwilson <tchoforwilson@gmail.com>
2024-04-19 07:43:29 +0100

    Included status bar header

--- src/navigations/FeedNavigator.js
+++ src/navigations/FeedNavigator.js
@@ -2,14 +2,21 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import { TasksScreen, TaskDetailsScreen } from "../screens";
+import { AppHeaderStyle } from "../components/header";
 
 const Stack = createStackNavigator();
 
 const FeedNavigator = () => {
   return (
-    <Stack.Navigator screenOptions={{ presentation: "modal" }}>
+    <Stack.Navigator
+      screenOptions={{ presentation: "modal", ...AppHeaderStyle }}
+    >
       <Stack.Screen name="Tasks" component={TasksScreen} />
-      <Stack.Screen name="TaskDetails" component={TaskDetailsScreen} />
+      <Stack.Screen
+        name="TaskDetails"
+        component={TaskDetailsScreen}
+        options={{ title: "Task details" }}
+      />
     </Stack.Navigator>
   );
 };
