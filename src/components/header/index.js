e2fd91fb894b54132f04b40101af21ac70ff1195
tchoforwilson <tchoforwilson@gmail.com>
2024-11-11 20:04:59 +0100

    App Header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,2 +1,3 @@
 export { default as AppHeaderStyle } from "./AppHeaderStyle";
 export { default as AuthHeading } from "./AuthHeading";
+export { default as TaskScreenHeader } from "./TaskScreenHeader";
