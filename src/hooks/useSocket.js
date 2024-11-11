90cc3196afbeffff9d406f6c9df0474a4f2fbec7
tchoforwilson <tchoforwilson@gmail.com>
2024-11-11 20:03:34 +0100

    Implemented socket hook

--- src/hooks/useSocket.js
+++ src/hooks/useSocket.js
@@ -1,35 +1,21 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import socket from "../services/socket";
-import useAuth from "./useAuth";
-import storage from "../context/storage";
 
 const useSocket = () => {
-  const [socketId, setSocketId] = useState(null);
-  const auth = useAuth();
-
-  const connect = () => {
-    socket.emit("connection");
-    socket.connect();
-    console.log(socket.id);
-  };
-
-  socket.on("connected", (socket) => {
-    console.log("Connected to server");
-    console.log("Socket ID:", socket.id);
-    storage.storeSocketId(socket.id);
-    auth.storeNewUser(user);
-    setSocketId(socket.id);
-  });
-
+  console.log(socket.id);
+  socket.connect();
+  console.log(socket.connected);
   useEffect(() => {
-    connect();
+    socket.on("disconnect", () => {
+      console.log("Disconnected from server");
+    });
 
     return () => {
       socket.disconnect();
     };
-  }, [auth, storage]);
+  }, []);
 
-  return socketId;
+  return { socket };
 };
 
 export default useSocket;
