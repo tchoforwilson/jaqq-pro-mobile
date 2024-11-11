73619736a8ba897aa1e0a6a076f6bb31b1ecf180
tchoforwilson <tchoforwilson@gmail.com>
2024-11-11 20:03:52 +0100

    Socket services

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,23 +1,27 @@
 import { io } from "socket.io-client";
-import storage from "../context/storage";
-import * as Device from "expo-device";
 import { Platform } from "react-native";
+import * as Device from "expo-device";
+import storage from "../context/storage";
 
-const handshakeToken = storage.getToken();
-let SOCKET_SERVER_URL = "http://10.0.2.2:9000";
+const handshakeToken = await storage.getToken(); // If getToken is async, use await
+let SOCKET_SERVER_URL;
+
 if (Platform.OS === "ios") {
   SOCKET_SERVER_URL = "http://localhost:9000";
-}
-if (Device.isDevice) {
+} else if (Platform.OS === "android" && !Device.isDevice) {
+  SOCKET_SERVER_URL = "http://10.0.2.2:9000";
+} else {
   SOCKET_SERVER_URL = "http://192.168.1.153:9000";
 }
 
-const socket = io(SOCKET_SERVER_URL, {
+console.log(SOCKET_SERVER_URL);
+
+const socket = io.connect(SOCKET_SERVER_URL, {
   transports: ["websocket"],
   auth: {
     token: handshakeToken,
   },
-  autoConnect: true,
+  autoConnect: false,
 });
 
 export default socket;
